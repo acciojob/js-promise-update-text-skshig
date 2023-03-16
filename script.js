1450be95f429984feaee99e6e4318db1b2d9ab72
Sakshi Gupta <43090350+skshig@users.noreply.github.com>
2023-03-16 15:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function myPromise(){
+  return new Promise(function(resolve,reject){
+    setTimeout(() =>{
+      console.log("hi")
+      resolve("Hello, world!");
+    },1000)
+  });
+}
+myPromise().then((value)=>{
+	console.log(value);
+}
